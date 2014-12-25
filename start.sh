9fcf5dce468c464f4f1a2524dfa571cf0f0f1397
LironHazan <liron@gigaspaces.com>
2014-12-25 17:18:45 +0200

    first commit

---
+++ start.sh
@@ -0,0 +1,26 @@
+
+LIGHT_GREEN='\x1B[1;32m'
+LIGHT_BLUE='\x1B[1;34m'
+CYAN='\x1B[1;36m'
+NC='\x1B[0m' # No Color
+RED='\x1B[0;31m'
+
+
+echo ""
+echo -e "${CYAN}"
+echo -e """  .o88b. db       .d88b.  db    db d8888b. d888888b d88888b db    db  """
+echo -e """ d8P  Y8 88      .8P  Y8. 88    88 88   8D    88    88       8b  d8   """
+echo -e """ 8P      88      88    88 88    88 88   88    88    88ooo     8bd8    """
+echo -e """ 8b      88      88    88 88    88 88   88    88    88~~~      88     """
+echo -e """ Y8b  d8 88booo.  8b  d8' 88b  d88 88  .8D   .88.   88         88     """
+echo -e """   Y88P  Y88888P   Y88P'  ~Y8888P' Y8888D' Y888888P YP         YP     """
+echo -e "${NC}"
+
+
+
+
+
+
+
+
+
